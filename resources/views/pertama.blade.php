15347e5846a5107fdb3df39928092f6999e8bba4
WilliamBryann <williambryanp77@gmail.com>
2025-05-15 16:01:45 +0700

    pertemuan 11

---
+++ resources/views/pertama.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Bantu Adaptasi Mahasiswa Baru, ITS Gelar Mentoring Agama Islam</title>
+        <link rel="stylesheet" href="mycss1.css">
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <h2><span style="color:rgb(2, 2, 104);">Bantu Adaptasi Mahasiswa Baru, ITS Gelar Mentoring Agama Islam</span></h2>
+        <h6>Oleh: itsash Source: ITS Online</h6>
+        <img src="https://www.its.ac.id/news/wp-content/uploads/sites/2/2025/02/IMG_20250223_084851-768x432.jpg" width="700">
+        <p class="judul">Dalam pembukaan acara, Ketua Tim Pembina Kerohanian Islam (TPKI) ITS Prof Dr Muhammad Mashuri MT menyampaikan bahwa mentoring merupakan sarana pencarian ilmu bagi mahasiswa.
+        “Kegiatan mentoring ini untuk mempersiapkan generasi emas di 2045 yang perlu dibekali dengan segudang ilmu, termasuk keilmuan Islam,” ujarnya.</p>
+        <p>Lebih lanjut, dosen yang akrab disapa Mashuri ini menjelaskan, kegiatan mentoring ini terdiri dari beberapa rangkaian acara. Adapun diawali dengan dengan kuliah umum yang menghadirkan empat materi utama dari dosen-dosen agama Islam ITS sebagai pondasi awal. Setelah mendapatkan pemahaman dasar, mahasiswa mengikuti kelas pendalaman dalam enam sesi yang mencakup diskusi dan ujian untuk menguji pemahaman mereka.</p>
+        <img src="https://www.its.ac.id/news/wp-content/uploads/sites/2/2025/02/IMG_20250223_101353-768x432.jpg" alt="gambar">
+    </body>
+</html>
